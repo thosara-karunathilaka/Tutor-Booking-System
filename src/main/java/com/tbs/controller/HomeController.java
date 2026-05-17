698960b722a7fb333d24686d178f42f0420ee344
Thosara Karunathilaka <thosarayethmal300@gmail.com>
2026-05-17 16:00:34 +0530

    added why choose us and student counts tutor count

--- src/main/java/com/tbs/controller/HomeController.java
+++ src/main/java/com/tbs/controller/HomeController.java
@@ -1,6 +1,8 @@
 package com.tbs.controller;
 
 import com.tbs.service.CourseService;
+import com.tbs.service.TutorService;
+import com.tbs.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,9 +14,17 @@ public class HomeController {
     @Autowired
     private CourseService courseService;
 
+    @Autowired
+    private TutorService tutorService;
+
+    @Autowired
+    private StudentService studentService;
+
     @GetMapping("/")
     public String home(Model model) {
         model.addAttribute("courses", courseService.getAllCourses());
+        model.addAttribute("tutorCount", tutorService.getAllTutors().size());
+        model.addAttribute("studentCount", studentService.getAllStudents().size());
         return "index";
     }
 }
